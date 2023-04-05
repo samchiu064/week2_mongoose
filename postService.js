1ef48a564164222cebeea45e4dc2f5531beab295
Sam Chiu <samchiu064@gmail.com>
2023-04-05 15:15:30 +0800

    style: adjust single quote to double quote

--- postService.js
+++ postService.js
@@ -1,93 +1,110 @@
-const http = require('http');
-const dotenv = require('dotenv').config({ path: "./config.env" });
-const mongoose = require('mongoose');
-const Post = require('./models/post');
-const errHandle = require('./errorHandle');
+const http = require("http");
+const dotenv = require("dotenv").config({ path: "./config.env" });
+const mongoose = require("mongoose");
+const Post = require("./models/post");
+const errHandle = require("./errorHandle");
 
-const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
+const DB = process.env.DATABASE.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
 
-mongoose.connect(DB).then(() => {
-  console.log('connect success');
-}).catch((err) => console.log(err));
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("connect success");
+  })
+  .catch((err) => console.log(err));
 
 const requestListener = async (req, res) => {
   let body = "";
-  req.on('data', (chunk) => body += chunk);
+  req.on("data", (chunk) => (body += chunk));
   const headers = {
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
-    'Content-Type': 'application/json'
+    "Access-Control-Allow-Headers":
+      "Content-Type, Authorization, Content-Length, X-Requested-With",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "PATCH, POST, GET,OPTIONS,DELETE",
+    "Content-Type": "application/json",
   };
 
   if (req.url === "/posts" && req.method === "GET") {
     const postData = await Post.find();
     res.writeHead(200, headers);
-    res.write(JSON.stringify({
-      status: true,
-      data: postData,
-    }))
+    res.write(
+      JSON.stringify({
+        status: true,
+        data: postData,
+      })
+    );
     res.end();
   } else if (req.url === "/posts" && req.method === "POST") {
-    req.on('end', async () => {
+    req.on("end", async () => {
       try {
         const data = JSON.parse(body);
         const postData = await Post.create({
-          ...data
+          ...data,
         });
         res.writeHead(200, headers);
-        res.write(JSON.stringify({
-          status: true,
-          data: postData,
-        }));
+        res.write(
+          JSON.stringify({
+            status: true,
+            data: postData,
+          })
+        );
         res.end();
       } catch (err) {
-        errHandle(res, err)
+        errHandle(res, err);
       }
-    })
+    });
   } else if (req.url === "/posts" && req.method === "DELETE") {
     const postData = await Post.deleteMany({});
     res.writeHead(200, headers);
-    res.write(JSON.stringify({
-      status: true,
-      data: postData,
-    }))
+    res.write(
+      JSON.stringify({
+        status: true,
+        data: postData,
+      })
+    );
     res.end();
-  } else if (req.url.startsWith('/posts/') && req.method === "DELETE") {
+  } else if (req.url.startsWith("/posts/") && req.method === "DELETE") {
     try {
-      const id = req.url.split('/').pop();
-      const postData = await Post.findByIdAndDelete(id)
+      const id = req.url.split("/").pop();
+      const postData = await Post.findByIdAndDelete(id);
 
       if (postData !== null) {
         res.writeHead(200, headers);
-        res.write(JSON.stringify({
-          status: true,
-          data: postData,
-        }))
+        res.write(
+          JSON.stringify({
+            status: true,
+            data: postData,
+          })
+        );
       } else {
         res.writeHead(404, headers);
-        res.write(JSON.stringify({
-          status: false,
-          message: '查無此 id'
-        }))
+        res.write(
+          JSON.stringify({
+            status: false,
+            message: "查無此 id",
+          })
+        );
       }
       res.end();
     } catch (err) {
-      errHandle(res, err)
+      errHandle(res, err);
     }
-  } else if (req.url.startsWith('/posts/') && req.method === "PATCH") {
-    req.on('end', async () => {
+  } else if (req.url.startsWith("/posts/") && req.method === "PATCH") {
+    req.on("end", async () => {
       try {
-        const id = req.url.split('/').pop();
+        const id = req.url.split("/").pop();
         const data = JSON.parse(body);
         const postData = await Post.findByIdAndUpdate(id, {
-          ...data
+          ...data,
         });
 
         let response = {
           status: false,
-          data: '',
-        }
+          data: "",
+        };
 
         if (Object.keys(data).length === 0) {
           response.data = "資料不得為空";
@@ -100,10 +117,10 @@ const requestListener = async (req, res) => {
 
         res.end(JSON.stringify(response));
       } catch (err) {
-        errHandle(res, err)
+        errHandle(res, err);
       }
-    })
+    });
   }
-}
+};
 
 const server = http.createServer(requestListener).listen(3005);
